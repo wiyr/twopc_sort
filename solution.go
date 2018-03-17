942a6b6c97199e38b138c32fdcea290df36c41c7
wiyr <nightwiyr@gmail.com>
2018-03-17 17:59:35 +0800

    init

---
+++ solution.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Solution struct {
+	que             *Queue
+	lastPrepareTime map[int64]time.Time
+	writeCh         chan data
+}
+
+type Queue struct {
+	buf []data
+}
+
+func (q *Queue) push(dat data, lastTime time.Time) {
+	q.buf = append(q.buf, dat)
+	i := len(q.buf) - 2
+	for ; i >= 0 && q.buf[i].sendTime.After(lastTime); i-- {
+		if q.buf[i].commit > q.buf[i+1].commit {
+			q.buf[i], q.buf[i+1] = q.buf[i+1], q.buf[i]
+		} else {
+			break
+		}
+	}
+}
+
+func (q *Queue) pop() {
+	q.buf = q.buf[1:]
+}
+
+func (q *Queue) front() data {
+	return q.buf[0]
+}
+
+func (q *Queue) empty() bool {
+	return len(q.buf) == 0
+}
+
+func NewOrderBuffer() *Solution {
+	return &Solution{
+		que:             &Queue{},
+		lastPrepareTime: map[int64]time.Time{},
+		writeCh:         make(chan data, 1000),
+	}
+}
+
+var finals []data
+var lastPut int
+
+func (o *Solution) putIt(dat data) {
+	var lastTime time.Time
+	if dat.kind == "commit" {
+		lastTime = o.lastPrepareTime[dat.prepare]
+		delete(o.lastPrepareTime, dat.prepare)
+	} else {
+		o.lastPrepareTime[dat.prepare] = dat.sendTime
+		return
+	}
+
+	//	log.Println(dat.commit, dat.sendTime.UnixNano(), lastTime.UnixNano(), dat.which)
+	o.que.push(dat, lastTime)
+
+	early := o.getEarliestPrepareTime()
+	for !o.que.empty() {
+		if o.que.front().sendTime.Before(early) {
+			/*
+			 *if len(finals) != 0 {
+			 *    if finals[len(finals)-1].commit > o.que.front().commit {
+			 *        log.Println("invalid")
+			 *    }
+			 *}
+			 *finals = append(finals, o.que.front())
+			 */
+			o.writeCh <- o.que.front()
+			o.que.pop()
+		} else {
+			break
+		}
+	}
+}
+
+func (o *Solution) writerDemon() {
+	filename := `./result.txt`
+	f, err := os.OpenFile(filename, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Panicf("open file %s failed: %s", filename, err)
+	}
+	defer f.Close()
+	for {
+		select {
+		case d := <-o.writeCh:
+			_, err := f.WriteString(strconv.FormatInt(d.commit, 10))
+			if err != nil {
+				log.Printf("[Error] write error %s", err)
+			}
+			f.WriteString("\n")
+		}
+	}
+}
+
+func (o *Solution) getEarliestPrepareTime() time.Time {
+	result := time.Now()
+	for _, t := range o.lastPrepareTime {
+		if result.After(t) {
+			result = t
+		}
+	}
+
+	return result
+}
+
+func (o *Solution) simpleSort() {
+	go o.writerDemon()
+	for {
+		select {
+		case dat := <-dataStreaming[0]:
+			o.putIt(dat)
+		case dat := <-dataStreaming[1]:
+			o.putIt(dat)
+		}
+	}
+}
---
+++ twopc_sort.go
@@ -0,0 +1,116 @@
+package main
+
+import (
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+var (
+	// number of client
+	clientNums = 2
+	// number of messages, simpify program implementation
+	messageNums = 200
+	// assume dataStreaming has unlimited capacity
+	dataStreaming []chan data
+
+	token int64
+	step  int64
+
+	maxSleepInterval int64 = 5
+	maxGap           int64 = 10
+
+	wg sync.WaitGroup
+)
+
+type data struct {
+	kind     string
+	prepare  int64
+	commit   int64
+	sendTime time.Time
+}
+
+func init() {
+	dataStreaming = make([]chan data, clientNums)
+	for i := 0; i < clientNums; i++ {
+		dataStreaming[i] = make(chan data, messageNums)
+	}
+}
+
+/* 1. please implement sort code
+*    u can add some auxiliary structures, variables and functions
+*    dont modify any definition
+* 2. implement flow control for the sort code
+ */
+func main() {
+	wg.Add(clientNums*2 + 1)
+	// genrateDatas and sort are parallel
+	for i := 0; i < clientNums; i++ {
+		go func(index int) {
+			defer wg.Done()
+			generateDatas(index)
+		}(i)
+		go func(index int) {
+			defer wg.Done()
+			generateDatas(index)
+		}(i)
+	}
+
+	go func() {
+		defer wg.Done()
+		sort()
+	}()
+	wg.Wait()
+}
+
+/*
+* 1 assume dataStreamings are endless => we have infinitely many datas;
+* because it's a simulation program, it has a limited number of datas, but the assumption is not shoul be satisfied
+* 2 sort commit kind of datas that are from multiple dataStreamings by commit ascending
+* and output them in the fastest way you can think
+ */
+func sort() {
+	solver := NewOrderBuffer()
+	solver.simpleSort()
+}
+
+/*
+* generate prepare and commit datas.
+* assume max difference of send time between prepare and commit data is 2*maxSleepInterval(millisecond),
+* thus u would't think some extreme cases about thread starvation.
+ */
+func generateDatas(index int) {
+	for i := 0; i < messageNums; i++ {
+		prepare := incrementToken()
+		sleep(maxSleepInterval)
+
+		dataStreaming[index] <- data{
+			kind:     "prepare",
+			prepare:  prepare,
+			sendTime: time.Now(),
+		}
+		sleep(maxSleepInterval)
+
+		commit := incrementToken()
+		sleep(maxSleepInterval)
+
+		dataStreaming[index] <- data{
+			kind:     "commit",
+			prepare:  prepare,
+			commit:   commit,
+			sendTime: time.Now(),
+		}
+		sleep(10 * maxSleepInterval)
+	}
+}
+
+func incrementToken() int64 {
+	return atomic.AddInt64(&token, rand.Int63()%maxGap+1)
+}
+
+func sleep(factor int64) {
+	interval := atomic.AddInt64(&step, 3)%factor + 1
+	waitTime := time.Duration(rand.Int63() % interval)
+	time.Sleep(waitTime * time.Millisecond)
+}
